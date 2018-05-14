f49988bfd55cea690e777eba4891b9ec1fe4ee9d
Amol <amol.jain1905@gmail.com>
2018-05-14 16:11:18 +1000

    made the datatbase and all required functions

--- movieHub/dbMake.py
+++ movieHub/dbMake.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dbFunctions import *
+import datetime
 
 Base = declarative_base()
 
@@ -15,7 +17,7 @@ class Movie(Base):
 
 class Cinema(Base):
     __tablename__ = "cinema"
-    cinema_id = Column(Integer, primary_key=True)
+    cinema_id = Column(Integer, primary_key=True , autoincrement=False)
     name = Column(String(100), nullable=False)
 
 class Plays(Base):
@@ -24,15 +26,15 @@ class Plays(Base):
     cinema_id = Column(Integer, ForeignKey("cinema.cinema_id"), nullable=False)
     imdb_id = Column(Integer, ForeignKey("movie.imdb_id"), nullable=False)
 
-class Showtime(Base):
-    __tablename__ = "showtime"
-    showtime_id = Column(Integer, primary_key=True)
-    time = Column(String(100), nullable=False)
+# class Showtime(Base):
+#     __tablename__ = "showtime"
+#     showtime_id = Column(Integer, primary_key=True)
+#     time = Column(String(100), nullable=False)
 
 class Time(Base):
     __tablename__ = "time"
     time_id = Column(Integer, primary_key=True)
-    showtime_id = Column(Integer, ForeignKey("showtime.showtime_id"), nullable=False)
+    showtime = Column(String(100), nullable=False)
     cinema_id = Column(Integer, ForeignKey("cinema.cinema_id"), nullable=False)
     imdb_id = Column(Integer, ForeignKey("movie.imdb_id"), nullable=False)
 
@@ -46,8 +48,8 @@ def addMovie(session, id, name, poster, is_showing):
     session.add(new_movie)
     session.commit()
 
-def addCinema(session, name):
-    new_cinema = Cinema(name=name)
+def addCinema(session, name_C , id_c):
+    new_cinema = Cinema(cinema_id=id_c , name=name_C)
     session.add(new_cinema)
     session.commit()
 
@@ -56,13 +58,13 @@ def addPlays(session, imdb_id, cinema_id):
     session.add(new_plays)
     session.commit()
 
-def addShowtime(session, time):
-    new_showtime = Showtime(time=time)
-    session.add(new_showtime)
-    session.commit()
+# def addShowtime(session, time):
+#     new_showtime = Showtime(time=time)
+#     session.add(new_showtime)
+#     session.commit()
 
-def addTime(session, imdb_id, cinema_id, showtime_id):
-    new_time = Time(imdb_id=imdb_id, cinema_id=cinema_id, showtime_id=showtime_id)
+def addTime(session, imdb_id, cinema_id, show_time):
+    new_time = Time(imdb_id=imdb_id, cinema_id=cinema_id, showtime=show_time)
     session.add(new_time)
     session.commit()
 
@@ -71,11 +73,59 @@ def addUser(session, username, password):
     session.add(new_user)
     session.commit()
 
+
+# this will add all the theaters supplied using the parameteres into the session 
+def addall_theaters(session ,listof_theaters):
+    for i in range(0, len(listof_theaters["cinemas"])):
+         # print("%s " % (listof_theaters["cinemas"][i]["name"]) , (listof_theaters["cinemas"][i]["id"]))
+         addCinema(session , listof_theaters["cinemas"][i]["name"] , (listof_theaters["cinemas"][i]["id"]))
+
+#this will add all the movies supplied into the session depending on if it is upcoming or not 
+def addall_movies(session , listof_movies , includes_upcoming):
+    for i in range(0, len(listof_movies["movies"])):
+        addMovie(session , 
+        int(get_imdbId(listof_movies["movies"][i]["id"])),
+        listof_movies["movies"][i]["title"] ,
+        listof_movies["movies"][i]["poster_image_thumbnail"] , includes_upcoming)
+
+#this will add all the showtimes and the times of the movies into the session 
+def addall_times(session , listof_times):
+    for i in range(0 , len(listof_times)["showtimes"]):
+        addTime(session , int(get_imdbId(len(listof_times["showtimes"][i]["movie_id"]))),
+            int(len(listof_times["showtimes"][i]["cinema_id"]))  , len(listof_times["showtimes"][i]["start_at"]))
+#goes through all the cinemas in the database and 
+def addall_plays(session):
+    theaters = session.query(Cinema).filter_by(id>0).all
+    for Cinema in theaters:
+       listof_movies =  get_movie(str(Cinema.cinema_id))
+       for i in range(0,len(listof_movies["movies"])):
+            addPlays(session ,int(get_imdbId(listof_movies["movies"][i]["movie_id"])) ,Cinema.cinema_id)
+
+
+
 engine = create_engine('sqlite:///movies.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
+#for upcomingdates
+date = datetime.date.today() + datetime.timedelta(days=7)
+
+#add Cinemas
+#the arguments location , distance 
+addall_theaters(session , get_theaters("-33.939961, 151.22966" , 5))
+#add Movies 
+#arguments you can add a theater_id to make it precise
+addall_movies(session , get_movie("") , False);
+addall_movies(session , get_all_upcoming(date))
+#add showtimes
+#arguments you can use cinema id or movie id aswell to make it more specific
+addall_times(session , get_Showtimes("" , ""))
+addall_plays(session)
+
+#
+
+
 
 # addMovie(session, 4154756, "Avengers: Infinity War", "http://image.tmdb.org/t/p/w154/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", True)
 # addMovie(session, 2231461, "Rampage", "http://image.tmdb.org/t/p/w154/30oXQKwibh0uANGMs0Sytw3uN22.jpg", True)
--- movieHub/showtimeAPI.py
+++ movieHub/showtimeAPI.py
@@ -3,14 +3,15 @@ import json
 from imdb import IMDb
 
 # sydney city id 7820
-def send_request():
+def get_showtimes(cinema_ids , movie_ids):
     try:
         response = requests.get(
-            url="https://api.internationalshowtimes.com/v4/cinemas/",
+            url="https://api.internationalshowtimes.com/v4/showtimes/",
             params={
-                "country": "AU",
-                "location": "-33.9, 151.2",
-                "distance" : 50
+                "country" : "AU",
+                "countries" : "AU",
+                "movie_id" : movie_ids,
+                "cinema_id" : cinema_ids
             },
             headers={
                 "X-API-Key": "jSaZYUE0VbpaOAUawsBmScBjQjXB5Vd8",
@@ -18,15 +19,82 @@ def send_request():
         )
         json_data = response.text
         loaded_json = json.loads(json_data)
-        for i in range(0, len(loaded_json["cinemas"])):
-            print("%s" % (loaded_json["cinemas"][i]["name"]))
-        # print('Response HTTP Status Code: {status_code}\n'.format(
-        #     status_code=response.status_code))
-        # print('Response HTTP Response Body: {content}\n'.format(
-        #     content=response.content))
-        # print(response.text)
+        return loaded_json
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
 
-send_request()
+# -33.939961, 151.22966
+def get_theaters(curr_location , distance_to_search):
+    try:
+        response = requests.get(
+            url="https://api.internationalshowtimes.com/v4/cinemas/",
+            params={
+                "country" : "AU",
+                "location" : curr_location,
+                "distance" : distance_to_search
+            },
+            headers={
+                "X-API-Key": "jSaZYUE0VbpaOAUawsBmScBjQjXB5Vd8",
+            },
+        )
+        json_data = response.text
+        loaded_json = json.loads(json_data)
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+    return loaded_json
+
+def get_movie(cinema_ids):
+    try:
+        response = requests.get(
+            url="https://api.internationalshowtimes.com/v4/movies/",
+            params={
+                "countries" : "AU",
+                "cinema_id" : cinema_ids                
+            },
+            headers={
+                "X-API-Key": "jSaZYUE0VbpaOAUawsBmScBjQjXB5Vd8",
+            },
+        )
+        json_data = response.text
+        loaded_json = json.loads(json_data)
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+
+def get_all_upcoming(curr_date):
+    try:
+        response = requests.get(
+            url="https://api.internationalshowtimes.com/v4/movies/",
+            params={
+                "countries" : "AU",
+                "release_date_from" : curr_date,
+                "include_upcomings" : "true"
+            },
+            headers={
+                "X-API-Key": "jSaZYUE0VbpaOAUawsBmScBjQjXB5Vd8",
+            },
+        )
+        json_data = response.text
+        loaded_json = json.loads(json_data)
+        return loaded_json
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+
+def get_imdbId(movie_ids):
+    rand = "https://api.internationalshowtimes.com/v4/movies/"
+    try:
+        response = requests.get(
+            url = rand + movie_ids,
+            params = {
+            },
+            headers = {
+                "X-API-Key": "jSaZYUE0VbpaOAUawsBmScBjQjXB5Vd8",
+            },
+        )
+        json_data = response.text
+        loaded_json = json.loads(json_data)
+        return loaded_json
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
 
